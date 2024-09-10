54e39522b7dc29ad66cd59f15bdefa7b88c2a504
lollipopwyn <rwewine@gmail.com>
2024-09-10 17:42:48 +0900

    first commit

---
+++ src/utils/apiUrl.js
@@ -0,0 +1,14 @@
+const GET_TASKS_API_URL = 'https://taskbackend.aiccchant.com/get_tasks';
+const POST_TASKS_API_URL = 'https://taskbackend.aiccchant.com/post_task';
+const DELETE_TASKS_API_URL = 'https://taskbackend.aiccchant.com/delete_task';
+const UPDATE_COMPLETED_TASKS_API_URL =
+  'https://taskbackend.aiccchant.com/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://taskbackend.aiccchant.com/update_task';
+
+export {
+  GET_TASKS_API_URL,
+  POST_TASKS_API_URL,
+  DELETE_TASKS_API_URL,
+  UPDATE_COMPLETED_TASKS_API_URL,
+  UPDATE_TASK_API_URL,
+};
